a134d0e714f5b2a1ec5a04dc4794d1c8f8bea907
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-30 09:34:55 +0630

    added legend scales

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function collectedData(error, education, county) {
                       .text('United States Educational Attainment')
     const description = main.append('div')
                             .attr('id', 'description')
-                            .text("Percentage of adults age 25 and older with a bachelor's degree or higher                         (2010-2014)")
+                            .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)")
 
     const w = 960;
     const h = 600;
@@ -34,5 +34,30 @@ function collectedData(error, education, county) {
                         .append('path')
                         .attr('class', 'county')
                         .attr('d', d3.geoPath())
+
+    const minMax = d3.extent(education.map(e=>e.bachelorsOrHigher/100));
+    const xScaleL = d3.scaleLinear()
+                      .domain(minMax)
+                      .range([600, 800])
+
+    function increment(scale) {
+      let arr = [];
+      console.log(scale)
+      for (let i = scale[0]; i <= scale[1]+0.091; i+=0.091) {
+        arr.push(i)
+      }
+      return arr;
+    }
+
+    const xAxisL = d3.axisBottom(xScaleL)
+                     .tickValues(increment(minMax))
+                     .tickFormat(d3.format(".0%"))
+    svg.append('g')
+       .attr('id', 'x-axis')
+       .attr('transform', 'translate(0,50)')
+       .call(xAxisL)
+
+    const legend = svg.append('g')
+                      .attr('id', 'legend')
 
 }
