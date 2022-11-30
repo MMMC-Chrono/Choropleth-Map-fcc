780a034be7f449d77fcafa887585a848cc23c2c3
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-30 10:00:33 +0630

    added counties color

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ d3.queue()
 
 function collectedData(error, education, county) {
     if (error) { return error};
-    console.log(education, county)
     const main = d3.select('body')
                    .append('div')
                    .attr('id', 'main')
@@ -26,7 +25,7 @@ function collectedData(error, education, county) {
                     .attr('width', w)
                     .attr('height', h)
                     .style('background', 'whitesmoke')
-        
+    const colorLtoH = ['#ffe6e6', '#ffb3b3', '#ff8080', '#ff4d4d', '#ff1a1a', '#e60000', '#b30000', '#800000'];
     const counties = svg.append('g')
                         .selectAll('path')
                         .data(topojson.feature(county, county.objects.counties).features)
@@ -34,16 +33,27 @@ function collectedData(error, education, county) {
                         .append('path')
                         .attr('class', 'county')
                         .attr('d', d3.geoPath())
-
+                        .data(education)
+                        .attr('fill', (education)=> {
+                          const e = education.bachelorsOrHigher
+                          return e > 3 && e <= 12 ? colorLtoH[0] :
+                          e > 12 && e <= 21 ? colorLtoH[1] :
+                          e > 21 && e <= 30 ? colorLtoH[2] :
+                          e > 30 && e <= 39 ? colorLtoH[3] : 
+                          e > 39 && e <= 48 ? colorLtoH[4] : 
+                          e > 48 && e <= 57 ? colorLtoH[5] : 
+                          e > 57 && e <= 66 ? colorLtoH[6] : colorLtoH[7]
+                          //3, 12, 21, 30, 39, 48, 57, 66, 75
+                        })
+                        //console.log(education)
     const minMax = d3.extent(education.map(e=>e.bachelorsOrHigher/100));
     const xScaleL = d3.scaleLinear()
                       .domain(minMax)
-                      .range([600, 800])
+                      .range([600, 850])
 
     function increment(scale) {
       let arr = [];
-      console.log(scale)
-      for (let i = scale[0]; i <= scale[1]+0.091; i+=0.091) {
+      for (let i = scale[0]; i <= scale[1] + 0.091; i += 0.091) {
         arr.push(i)
       }
       return arr;
@@ -53,7 +63,6 @@ function collectedData(error, education, county) {
                      .tickValues(increment(minMax))
                      .tickFormat(d3.format(".0%"))
     svg.append('g')
-       .attr('id', 'x-axis')
        .attr('transform', 'translate(0,50)')
        .call(xAxisL)
 
